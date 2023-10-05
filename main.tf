2e3a7b939e4975dd576298a8ea5b85dece8f1f7a
adeeb <adeeb.s2547@gmail.com>
2023-10-05 21:56:09 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ resource "aws_instance" "terraform_new" {
     ami           = "ami-0261755bbcb8c4a84"
     instance_type = "t2.micro"
     subnet_id = "subnet-03fc7030febd7d21c"
-    key_name = "adeeb-devops.pem"
+    key_name = "adeeb-devops"
 }
